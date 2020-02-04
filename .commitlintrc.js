79f3330f515fe36e3d99cf570d2bde8b89e65aa2
Sai krishna <saikrishna63@gmail.com>
2020-02-04 22:34:43 +0530

    retesting

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
 	rules: {
-		"body-leading-blank": [1, 'always'],
+		"body-leading-blank": [2, 'never'],
 		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 1],
 		'scope-case': [2, 'always', 'lower-case'],
 		'subject-case': [
 			2,
 			'never',
 			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
 		],
-		'subject-empty': [2, 'always'],
+		'subject-empty': [2, 'never'],
 		'subject-full-stop': [2, 'never', '.'],
 		'type-case': [2, 'always', 'lower-case'],
 		'type-empty': [2, 'never'],
 		'type-enum': [
 			2,
-			'always',
+			'never',
 			[
 				'build',
 				'chore',
